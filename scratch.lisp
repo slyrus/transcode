99fbade2d2abef7e059bd87404a8c3b1c4148b09
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-12-09 11:16:57 -0800

    removed iso-media stuff

--- scratch.lisp
+++ scratch.lisp
@@ -2,37 +2,13 @@
 (in-package #:transcode)
 
 (cl:defpackage #:transcode-scratch
-  (:use #:cl #:transcode))
+  (:use #:cl #:transcode #:iso-media))
 
 (cl:in-package #:transcode-scratch)
 
 (defparameter *test-file*
-  #P"/mnt/iTunes_Music/ALAC/The Microscopic Septet/Friday The Thirteenth_ The Micros Play Monk/01 Brilliant Corners.m4a")
+  #P"/Volumes/iTunes_Music/ALAC/The Microscopic Septet/Friday The Thirteenth_ The Micros Play Monk/01 Brilliant Corners.m4a")
 
 (let ((file *test-file*))
   (read-iso-media-file file))
-
-(let ((file *test-file*))
-  (iso-media-box-data
-   (first
-    (remove-if-not (lambda (box)
-                     (equalp (iso-media-box-type box)
-                             (map 'vector #'char-code "moov")))
-                   (read-iso-media-file file)))))
-
-(let ((file *test-file*))
-  (find-box-type "mvhd"
-                 (iso-media-box-data
-                  (find-box-type "moov" (read-iso-media-file file)))))
-
-(let ((file *test-file*))
-  (map 'string #'code-char
-       (iso-media-box-data
-        (find-box-type "udta"
-                       (iso-media-box-data
-                        (find-box-type "moov" (read-iso-media-file file)))))))
-
-(let ((file *test-file*))
-  (iso-media-box-data
-   (find-box-type "moov" (read-iso-media-file file))))
 
