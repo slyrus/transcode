a23f4ee3764e502b77154c43b9e0cc481f7f0b08
Cyrus Harmon <cyrus@bobobeach.com>
2010-12-10 12:00:41 -0800

    start getting ready to handle multiple file types besides just mp4

--- scratch.lisp
+++ scratch.lisp
@@ -1,5 +1,6 @@
 
-(in-package #:transcode)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (asdf:oos 'asdf:load-op 'transcode))
 
 (cl:defpackage #:transcode-scratch
   (:use #:cl #:transcode #:iso-media))
@@ -7,7 +8,7 @@
 (cl:in-package #:transcode-scratch)
 
 (defparameter *test-file*
-  #P"/Volumes/iTunes_Music/ALAC/The Microscopic Septet/Friday The Thirteenth_ The Micros Play Monk/01 Brilliant Corners.m4a")
+  #P"/mnt/iTunes_Music/ALAC/The Microscopic Septet/Friday The Thirteenth_ The Micros Play Monk/01 Brilliant Corners.m4a")
 
 (let ((file *test-file*))
   (read-iso-media-file file))
--- transcode.lisp
+++ transcode.lisp
@@ -1,3 +1,6 @@
+;;;
+;;; TODO: get mp3, mp4 or AAC stream as appropriate!
+;;;
 
 (eval-when (:compile-toplevel :load-toplevel)
   (asdf:oos 'asdf:load-op 'iso-media)
@@ -15,20 +18,27 @@
 (defparameter *audio-source-root-directory* #p"/mnt/iTunes_Music/")
 (defparameter *audio-destination-root-directory* #p"/mnt/iTunes_Music/AAC/")
 
-(defun run-audio-decoder (src &key (input-type :apple-lossless))
-  (case input-type
-    (:apple-lossless (sb-ext:run-program "/usr/bin/alac-decoder"
+(defun run-audio-decoder (src &key (input-type "alac"))
+  (case (intern input-type)
+    (#.(intern "alac") (sb-ext:run-program "/usr/bin/alac-decoder"
                                          (list (sb-ext:native-namestring src))
                                          :output :stream
                                          :wait nil))))
 
-(defun run-audio-encoder (input dest &key (output-type :aac))
-  (case output-type
-    (:aac (sb-ext:run-program "/usr/bin/faac"
+(defun run-audio-encoder (input dest &key (output-type "mp4a"))
+  (case (intern output-type)
+    (#.(intern "mp4a") (sb-ext:run-program "/usr/bin/faac"
                       `("-o"
                         ,(sb-ext:native-namestring dest)
                         "-")
                       :input input))))
+
+ (defun recursively-list-files (dir &key test)
+   (let ((files))
+     (apply #'fad:walk-directory dir (lambda (f) (push f files))
+            (append '(:directories nil)
+                    (when test `(:test ,test))))
+     (nreverse files)))
 
 (defun convert-audio-directory (srcdir &key input-type output-type)
   (let ((files
@@ -39,9 +49,16 @@
               (let ((dest (merge-pathnames (enough-namestring src *audio-source-root-directory*)
                                            *audio-destination-root-directory*)))
                 (ensure-directories-exist dest)
-                (let* ((decoder (apply #'run-audio-decoder
+                (cond
+                  ((equal (pathname-type src) "mp3")
+                   nil)
+                  ((equal (pathname-type src) "flac")
+                   nil)
+                  ((find (pathanme-type src) '("mp4" "m4a") :test 'equal)))
+                (let* ((input-type (or input-type (audio-sample-type (read-iso-media-file src)))) 
+                       (decoder (apply #'run-audio-decoder
                                        src
-                                       (when input-type (list input-type))))
+                                       (when input-type (list :input-type input-type))))
                        (encoder (apply #'run-audio-encoder 
                                        (sb-ext:process-output decoder)
                                        dest
@@ -55,5 +72,27 @@
                           (fad:copy-stream (sb-ext:process-error encoder) str)
                           str))
                         (t dest)))))
-            files)))
+            (subseq files 0 1))))
 
+(defun read-audio-directory-info (srcdir &key debug-limit)
+  (let ((files
+         (recursively-list-files
+          srcdir
+          :test (lambda (x) (not (equal "._" (subseq (pathname-name x) 0 2)))))))
+    (mapcar (lambda (file)
+              (let ((iso-media (read-iso-media-file file)))
+                (audio-sample-type iso-media)))
+            (if debug-limit
+                (subseq files 0 debug-limit)
+                files))))
+
+(defun temp-list-files (srcdir &key debug-limit)
+  (let ((files
+         (recursively-list-files
+          srcdir
+          :test (lambda (x) (not (equal "._" (subseq (pathname-name x) 0 2)))))))
+    (mapcar (lambda (file)
+              (pathname-type file))
+            (if debug-limit
+                (subseq files 0 debug-limit)
+                files))))
