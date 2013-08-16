da27c22047f1b55e51f4e2d4040aa135eacb7cbc
Cyrus Harmon <cyrus@bobobeach.com>
2013-08-16 11:28:07 -0700

    export some file handling functions
     * is-file-p, recursively-list-files, apple-cruft-file-p, remove-apple-cruft-files

--- package.lisp
+++ package.lisp
@@ -4,5 +4,10 @@
   (:export #:run-audio-decoder
            #:run-audio-encoder
            
-           #:convert-audio-directory))
+           #:convert-audio-directory
+
+           #:is-file-p
+           #:recursively-list-files
+           #:apple-cruft-file-p
+           #:remove-apple-cruft-files))
 
